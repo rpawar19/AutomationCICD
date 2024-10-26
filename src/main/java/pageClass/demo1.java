f692b82be3e3b2cc3e3368da688154333aba0ef8
rpawar19 <rajendra2000@ymail.com>
2024-10-26 16:22:48 +0530

    testcase 2 with changes

--- src/main/java/pageClass/demo1.java
+++ src/main/java/pageClass/demo1.java
@@ -1,12 +1,16 @@
 package pageClass;
 
 import Base.Base;
+import dev.failsafe.internal.util.Assert;
 
 public class demo1 extends Base {
 
     public void openSite(){
-        webdriver.get("https://google.com");
+        webdriver.get("https:www.//google.com");
 
+    }
+    public void openSite2(){
+        webdriver.get("https:www.vipsha.com");
     }
     public void getURL(){
         String url = webdriver.getCurrentUrl();
--- src/test/java/testClass/demo1test.java
+++ src/test/java/testClass/demo1test.java
@@ -5,21 +5,24 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import pageClass.demo1;
 
+import java.security.PublicKey;
+
 public class demo1test extends Base {
 
     public static demo1 demo = new demo1();
-
+    public static String currenturl;
     @Test
     public void testcase1(){
         initWebDriver();
         demo.openSite();
-        demo.getURL();
+        currenturl=webdriver.getCurrentUrl();
+        Assert.assertTrue(currenturl.contains("https://www.google.com"));
     }
     @Test
     public void testcase2(){
-        demo.openSite();
-        pause(10000);
+        demo.openSite2();
         System.out.printf("This is from TestCase2");
-        Assert.assertTrue(false);
+        currenturl=webdriver.getCurrentUrl();
+        Assert.assertTrue(currenturl.contains("https://www.vipsha.com"));
     }
 }
